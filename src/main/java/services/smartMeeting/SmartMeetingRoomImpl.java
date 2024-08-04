a4f72b242f40447a520f7919b228fef9a8a0b819
seanwelch <seanwelch51199@icloud.com>
2024-08-04 13:08:04 +0100

    redo meeting room later

--- src/main/java/services/smartMeeting/SmartMeetingRoomImpl.java
+++ src/main/java/services/smartMeeting/SmartMeetingRoomImpl.java
@@ -59,10 +59,10 @@ public class SmartMeetingRoomImpl extends SmartMeetingRoomGrpc.SmartMeetingRoomI
         String timeSlot = request.getTimeSlot();
 
         String roomQuery = "select status from room_details where room_id = ?";
-        String bookingQuery = "insert into booking (roomId, userId, timeslot) values (?, ?, ?)";
+        String bookingQuery = "insert into booking (room_id, user_id, time_slot) values (?, ?, ?)";
 
         try (PreparedStatement roomStmt = conn.prepareStatement(roomQuery)) {
-            roomStmt.setString(1, roomId);
+            roomStmt.setInt(1, roomId);
             ResultSet roomResult = roomStmt.executeQuery();
             if (!roomResult.next()) return "No Room found with ID: " + roomId;
 
@@ -74,7 +74,7 @@ public class SmartMeetingRoomImpl extends SmartMeetingRoomGrpc.SmartMeetingRoomI
             }
 
             try (PreparedStatement bookingStmt = conn.prepareStatement(bookingQuery)) {
-                bookingStmt.setString();
+//                bookingStmt.setString();
 
                 return "booking complete";
             } catch (SQLException e) {
@@ -89,51 +89,51 @@ public class SmartMeetingRoomImpl extends SmartMeetingRoomGrpc.SmartMeetingRoomI
     }
 
     private boolean cancelBookingLogic(String bookingId) {
-        for (Map<services.smartMeeting.Timestamp, String> roomBookings : bookings.values()) {
-            if (roomBookings.containsValue(bookingId)) {
-                roomBookings.values().removeIf(id -> id.equals(bookingId));
-                return true;
-            }
-        }
+//        for (Map<services.smartMeeting.Timestamp, String> roomBookings : bookings.values()) {
+//            if (roomBookings.containsValue(bookingId)) {
+//                roomBookings.values().removeIf(id -> id.equals(bookingId));
+//                return true;
+//            }
+//        }
         return false;
     }
 
     private AvailabilityResponse checkAvailabilityLogic(CheckAvailabilityRequest request) {
-        String roomId = request.getRoomId();
-        services.smartMeeting.Timestamp timeSlot = request.getTimeSlot();
-
-        if (!rooms.containsKey(roomId)) {
-            return AvailabilityResponse.newBuilder()
-                    .setSuccess(false)
-                    .setStatus(RoomStatus.UNAVAILABLE)
-                    .build();
-        }
-
-        RoomDetails roomDetails = rooms.get(roomId);
-        RoomStatus status = bookings.get(roomId).containsKey(timeSlot) ? RoomStatus.OCCUPIED : RoomStatus.AVAILABLE;
-
-        List<services.smartMeeting.Timestamp> availableTimes = new ArrayList<>();
-
-        if (status == RoomStatus.OCCUPIED) {
-            Timestamp googleTimestamp = timeSlot.getValue();
-
-            Timestamp newGoogleTimestamp = Timestamp.newBuilder()
-                    .setSeconds(googleTimestamp.getSeconds() + 3600)
-                    .setNanos(googleTimestamp.getNanos())
-                    .build();
-
-            services.smartMeeting.Timestamp newTimeSlot = services.smartMeeting.Timestamp.newBuilder()
-                    .setValue(newGoogleTimestamp)
-                    .build();
-
-            availableTimes.add(newTimeSlot);
-        }
+//        int roomId = request.getRoomId();
+//        String timeSlot = request.getTimeSlot();
+//
+//        if (!rooms.containsKey(roomId)) {
+//            return AvailabilityResponse.newBuilder()
+//                    .setSuccess(false)
+//                    .setStatus(RoomStatus.UNAVAILABLE)
+//                    .build();
+//        }
+//
+//        RoomDetails roomDetails = rooms.get(roomId);
+//        RoomStatus status = bookings.get(roomId).containsKey(timeSlot) ? RoomStatus.OCCUPIED : RoomStatus.AVAILABLE;
+//
+//        List<services.smartMeeting.Timestamp> availableTimes = new ArrayList<>();
+//
+//        if (status == RoomStatus.OCCUPIED) {
+//            Timestamp googleTimestamp = timeSlot.getValue();
+//
+//            Timestamp newGoogleTimestamp = Timestamp.newBuilder()
+//                    .setSeconds(googleTimestamp.getSeconds() + 3600)
+//                    .setNanos(googleTimestamp.getNanos())
+//                    .build();
+//
+//            services.smartMeeting.Timestamp newTimeSlot = services.smartMeeting.Timestamp.newBuilder()
+//                    .setValue(newGoogleTimestamp)
+//                    .build();
+//
+//            availableTimes.add(newTimeSlot);
+//        }
 
         return AvailabilityResponse.newBuilder()
-                .setSuccess(true)
-                .setStatus(status)
-                .setDetails(roomDetails)
-                .addAllAvailableTimes(availableTimes)
+//                .setSuccess(true)
+//                .setStatus(status)
+//                .setDetails(roomDetails)
+//                .addAllAvailableTimes(availableTimes)
                 .build();
     }
 
