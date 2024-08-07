9871096c857c658400c7cae31deba186eba97574
seanwelch <seanwelch51199@icloud.com>
2024-08-07 20:01:14 +0100

    include service register in grpc server

--- src/main/java/services/GrpcServer.java
+++ src/main/java/services/GrpcServer.java
@@ -3,6 +3,7 @@ package services;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import services.database.Database;
+import services.serviceRegister.ServiceRegister;
 import services.smartAccess.SmartAccessControlImpl;
 import services.smartCoffee.SmartCoffeeMachineImpl;
 import services.smartMeeting.SmartMeetingRoomImpl;
@@ -19,6 +20,18 @@ public class GrpcServer {
                         .addService(new SmartCoffeeMachineImpl(conn))
                         .addService(new SmartMeetingRoomImpl(conn))
                         .build();
+
+                ServiceRegister serviceRegister = new ServiceRegister();
+                serviceRegister.registerService("SmartMeetingService");
+                serviceRegister.registerService("SmartCoffeeService");
+                serviceRegister.registerService("SmartAccessService");
+
+                Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                    System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                    server.shutdown();
+                    serviceRegister.unregisterService();
+                    System.err.println("*** server shut down");
+                }));
 
                 server.start();
                 System.out.println("Server started on port 8080");
