d8fa799a72ae1dc2138698e0ea95d7f0c0461213
seanwelch <seanwelch51199@icloud.com>
2024-08-08 20:26:47 +0100

    random signing key

--- src/main/java/services/constants/Constants.java
+++ src/main/java/services/constants/Constants.java
@@ -3,10 +3,13 @@ package services.constants;
 import io.grpc.Context;
 import io.grpc.Metadata;
 
+import java.security.SecureRandom;
+import java.util.Base64;
+
 import static io.grpc.Metadata.ASCII_STRING_MARSHALLER;
 
 public class Constants {
-    public static final String JWT_SIGNING_KEY = "L8hHXsaQOUjk5rg7XPGv4eL36anlCrkMz8CJ0i/8E/0=";
+    public static final String JWT_SIGNING_KEY = generateRandomSigningKey();
     public static final String BEARER_TYPE = "Bearer";
 
     public static final Metadata.Key<String> AUTHORIZATION_METADATA_KEY = Metadata.Key.of("Authorization", ASCII_STRING_MARSHALLER);
@@ -14,5 +17,17 @@ public class Constants {
 
     private Constants() {
         throw new AssertionError();
+    }
+
+    private static String generateRandomSigningKey() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] key = new byte[32];
+        secureRandom.nextBytes(key);
+        return Base64.getEncoder().encodeToString(key);
+    }
+
+    public static void main(String[] args) {
+        // For testing purposes
+        System.out.println("Generated JWT Signing Key: " + JWT_SIGNING_KEY);
     }
 }
