28d2c730cedbd51ae217670404500261d9b83acd
seanwelch <seanwelch51199@icloud.com>
2024-08-10 10:35:06 +0100

    refactor coffee ui

--- src/main/java/client/CoffeeUI.java
+++ src/main/java/client/CoffeeUI.java
@@ -3,17 +3,24 @@ package client;
 import javax.swing.*;
 import java.awt.*;
 
+import static client.StyledButtonUI.getjButton;
+
 public class CoffeeUI extends JPanel {
     public CoffeeUI(GrpcClient grpcClient, ClientUI parent) {
-        setBorder(BorderFactory.createTitledBorder("Coffee Machine"));
+        setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.WHITE), "Coffee Machine", 0, 0, null, Color.WHITE));
         setLayout(new FlowLayout());
+        setBackground(new Color(40, 40, 40));
 
-        JButton brewCoffeeButton = new JButton("Brew Coffee");
+        JButton brewCoffeeButton = createStyledButton("Brew Coffee");
         brewCoffeeButton.addActionListener(e -> {
             String response = grpcClient.brewCoffee();
             parent.displayResponse(response, response.contains("Error"));
         });
 
         add(brewCoffeeButton);
+    }
+
+    private JButton createStyledButton(String text) {
+        return getjButton(text);
     }
 }
