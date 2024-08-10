ed55330f3954ca5aad2ddfc09e553a8e55700ed0
seanwelch <seanwelch51199@icloud.com>
2024-08-10 12:23:21 +0100

    fix grpc client with new constructor and methods

--- src/main/java/client/services/GrpcClient.java
+++ src/main/java/client/services/GrpcClient.java
@@ -3,17 +3,20 @@ package client.services;
 import io.grpc.*;
 import services.constants.Constants;
 import services.smartAccess.AccessLevel;
+import services.smartAccess.AccessLogsResponse;
 import services.smartAccess.SmartAccessControlGrpc;
 import services.smartCoffee.CoffeeType;
 import services.smartCoffee.SmartCoffeeMachineGrpc;
 import services.smartMeeting.SmartMeetingRoomGrpc;
 import services.utils.JwtUtility;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
 
 public class GrpcClient {
     private final ManagedChannel channel;
-    private final AccessControlService accessControlService;
+    private final AccessControlClientService accessControlService;
     private final CoffeeMachineService coffeeMachineService;
     private final MeetingRoomService meetingRoomService;
 
@@ -40,7 +43,10 @@ public class GrpcClient {
                 .intercept(authInterceptor)
                 .build();
 
-        accessControlService = new AccessControlService(SmartAccessControlGrpc.newBlockingStub(channel));
+        SmartAccessControlGrpc.SmartAccessControlBlockingStub blockingStub = SmartAccessControlGrpc.newBlockingStub(channel);
+        SmartAccessControlGrpc.SmartAccessControlStub asyncStub = SmartAccessControlGrpc.newStub(channel);
+
+        accessControlService = new AccessControlClientService(blockingStub, asyncStub);
         coffeeMachineService = new CoffeeMachineService(SmartCoffeeMachineGrpc.newBlockingStub(channel));
         meetingRoomService = new MeetingRoomService(SmartMeetingRoomGrpc.newBlockingStub(channel));
     }
@@ -49,14 +55,25 @@ public class GrpcClient {
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
 
+    // Access control methods
     public String accessControl(int userId, AccessLevel level) {
         return accessControlService.unlockDoor(userId, level);
     }
 
+    public String raiseAlarm(int doorId, int userId, AccessLevel level) {
+        return accessControlService.raiseAlarm(doorId, userId, level);
+    }
+
+    public List<AccessLogsResponse> getAccessLogs(int doorId, String startTime, String endTime) {
+        return accessControlService.getAccessLogs(doorId, startTime, endTime);
+    }
+
+    // Coffee Machine Methods
     public String brewCoffee(CoffeeType coffeeType) {
         return coffeeMachineService.brewCoffee(coffeeType);
     }
 
+    // Meeting Room Methods
     public String bookRoom(int roomId, int userId, String time) {
         return meetingRoomService.bookRoom(roomId, userId, time);
     }
