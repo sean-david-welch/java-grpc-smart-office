3b255a799d12aa56a0872b73b754dea36e07bc19
seanwelch <seanwelch51199@icloud.com>
2024-08-10 10:35:13 +0100

    refactor access control ui

--- src/main/java/client/AccessControlUI.java
+++ src/main/java/client/AccessControlUI.java
@@ -3,12 +3,15 @@ package client;
 import javax.swing.*;
 import java.awt.*;
 
+import static client.StyledButtonUI.getjButton;
+
 public class AccessControlUI extends JPanel {
     public AccessControlUI(GrpcClient grpcClient, ClientUI parent) {
-        setBorder(BorderFactory.createTitledBorder("Access Control"));
+        setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.WHITE), "Access Control", 0, 0, null, Color.WHITE));
         setLayout(new FlowLayout());
+        setBackground(new Color(40, 40, 40)); // Dark background
 
-        JButton accessButton = new JButton("Unlock Door");
+        JButton accessButton = createStyledButton("Unlock Door");
         accessButton.addActionListener(e -> {
             String response = grpcClient.accessControl();
             parent.displayResponse(response, response.contains("Error"));
@@ -16,4 +19,10 @@ public class AccessControlUI extends JPanel {
 
         add(accessButton);
     }
+
+    private JButton createStyledButton(String text) {
+        return getjButton(text);
+    }
+
+
 }
