4f3dc7470c0d194b31019f15676f7aad104ceb6e
seanwelch <seanwelch51199@icloud.com>
2024-08-10 09:43:40 +0100

    client is working, javafx is a no no for now

--- src/main/java/client/ClientUI.java
+++ src/main/java/client/ClientUI.java
@@ -2,8 +2,6 @@ package client;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class ClientUI extends JFrame {
     private final GrpcClient grpcClient;
@@ -12,29 +10,22 @@ public class ClientUI extends JFrame {
     public ClientUI() {
         grpcClient = new GrpcClient("localhost", 8080);
 
-        // Set up the JFrame
         setTitle("gRPC Client UI");
         setSize(300, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
 
-        // Create UI components
         responseLabel = new JLabel("Response will be displayed here");
 
         JButton accessButton = new JButton("Unlock Door");
-        accessButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String response = grpcClient.accessControl();
-                responseLabel.setText("Access control response: " + response);
-            }
+        accessButton.addActionListener(e -> {
+            String response = grpcClient.accessControl();
+            responseLabel.setText("Access control response: " + response);
         });
 
-        // Add components to the JFrame
         add(accessButton);
         add(responseLabel);
 
-        // Make the window visible
         setVisible(true);
     }
 
@@ -43,21 +34,18 @@ public class ClientUI extends JFrame {
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                ClientUI clientUI = new ClientUI();
-                clientUI.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-                        try {
-                            clientUI.shutdown();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+        SwingUtilities.invokeLater(() -> {
+            ClientUI clientUI = new ClientUI();
+            clientUI.addWindowListener(new java.awt.event.WindowAdapter() {
+                @Override
+                public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                    try {
+                        clientUI.shutdown();
+                    } catch (InterruptedException e) {
+                        System.out.println("Something went wrong!");
                     }
-                });
-            }
+                }
+            });
         });
     }
 }
